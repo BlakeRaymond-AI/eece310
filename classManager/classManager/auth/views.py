f27fc0ffe020742c52196d15b7d28cd28c5b5613
Blake <blake.raymond@aquaticinformatics.com>
2014-03-29 18:17:52 -0700

    Check if user exists

--- classManager/classManager/auth/views.py
+++ classManager/classManager/auth/views.py
@@ -7,7 +7,7 @@ from django.template import loader, RequestContext
 
 def register_user(request):
     state = "Please register below..."
-    username = password = str()
+    username = email = first_name = last_name = ""
     if request.POST:
         username = request.POST.get('username')
         email = request.POST.get('email')
@@ -16,16 +16,26 @@ def register_user(request):
         last_name = request.POST.get('last_name')
 
         try:
+            user = User.objects.get(username=username)
+            state = "Failed to register: username %s already exists." % username
+        except User.DoesNotExist:
             user = User.objects.create_user(username, email=email, password=password)
             user.first_name = first_name
             user.last_name = last_name
             user.save()
-
             state = "You have successfully registered."
-        except:
-            state = "Failed to register."
+        except Exception as e:
+            state = "Error encountered while trying to register: %s" % str(e)
 
-    return render_to_response('register.html', {'state': state, 'username': username}, context_instance=RequestContext(request))
+    form = {
+        'state': state,
+        'username': username,
+        'email': email,
+        'first_name': first_name,
+        'last_name':last_name
+    }
+    return render_to_response('register.html', form, context_instance=RequestContext(request))
+
 
 def login_user(request):
     state = "Please log in below..."
